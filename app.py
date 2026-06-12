690990b095fe03ce9ca099531b1c3d8669a7503c
Rickette <rickettepk@gmail.com>
2026-06-12 17:29:14 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import qrcode
+
+st.write("Welcome to the QR Code Generator!")
+st.write("This app allows you to create QR codes for any link you want. Simply enter the link and click the button to generate your QR code. You can also customize the color of your QR code using the color picker below.")
+colours = st.color_picker("Pick a color for your QR code:", "#000000",
+                          help="Choose a color for your QR code. The default is black.")
+
+st.set_page_config(page_title="QR Code Generator",
+                   page_icon=":camera:", layout="centered")
+
+st.title("QR Code Generator")
+
+qr_data = st.text_input("Enter the link to generate QR code:")
+
+if st.button("Generate QR Code"):
+    if qr_data:
+        img = qrcode.make(qr_data)
+        img.save("qr_code.png")
+        st.image(image="qr_code.png", caption="Generated QR Code")
+    else:
+        st.warning("Please enter some data to generate a QR code.")
+st.write("Created by RickTech")
